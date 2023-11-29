d2ba82ffc4d41c1b3bdc8fe96f4c783afcd1b1d0
xiaws <xiaws.si@vsc.com>
2023-11-29 16:22:43 +0800

    Handling of duplicate requests (针对重复请求的处理)

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -4,61 +4,117 @@ import { ElMessage } from "element-plus"
 import { get, merge } from "lodash-es"
 import { getToken } from "./cache/cookies"
 
-/** 退出登录并强制刷新页面（会重定向到登录页） */
 function logout() {
   useUserStoreHook().logout()
-  location.reload()
+  // location.reload();
 }
 
-/** 创建请求实例 */
+/**
+ *  @param {Set} requestSet 请求的唯一标识集合
+ *  @param {Map} requestMap 请求的配置信息、url 和时间戳
+ *  @param {number} requestInterval 间隔时间(ms) 小于此时间视为重复提交
+ *  @param {Array} methodList 需要处理的请求类型数组
+ *  @param {Array} whiteList 需要处理的请求类型白名单api
+ */
+const requestSet = new Set<string>()
+const requestMap = new Map<string, { url: string; timestamp: number }>()
+const requestInterval = 1000
+const methodList = ["post", "put"]
+const whiteList = [""]
+const NOT_FOUND = Symbol()
+
+/** 获取请求的唯一标识 */
+function getRequestIdentifier(config: AxiosRequestConfig): string | typeof NOT_FOUND {
+  if (methodList.includes(config.method as string) && !whiteList.includes(config.url as string)) {
+    return `${config.method}-${config.url}`
+  }
+  return NOT_FOUND
+}
+
 function createService() {
-  // 创建一个 axios 实例命名为 service
   const service = axios.create()
-  // 请求拦截
+
   service.interceptors.request.use(
-    (config) => config,
-    // 发送失败
+    (config) => {
+      const requestIdentifier = getRequestIdentifier(config)
+
+      if (requestIdentifier !== NOT_FOUND) {
+        const currentTime = Date.now()
+        if (requestSet.has(requestIdentifier)) {
+          const previousRequest = requestMap.get(requestIdentifier)
+
+          logRequestDetails(previousRequest, currentTime, requestInterval)
+
+          if (currentTime - previousRequest!.timestamp <= requestInterval) {
+            const message = "数据正在处理，请勿重复提交"
+            ElMessage.error(message)
+            return Promise.reject(new Error(message))
+          }
+        }
+
+        requestSet.add(requestIdentifier)
+        requestMap.set(requestIdentifier, {
+          timestamp: currentTime,
+          url: config.url as string
+        })
+      }
+
+      return config
+    },
     (error) => Promise.reject(error)
   )
-  // 响应拦截（可根据具体业务作出相应的调整）
+
   service.interceptors.response.use(
     (response) => {
-      // apiData 是 api 返回的数据
+      const requestIdentifier = getRequestIdentifier(response.config)
+
+      if (requestIdentifier !== NOT_FOUND) {
+        requestSet.delete(requestIdentifier)
+        requestMap.delete(requestIdentifier)
+      }
+
       const apiData = response.data
-      // 二进制数据则直接返回
       const responseType = response.request?.responseType
-      if (responseType === "blob" || responseType === "arraybuffer") return apiData
-      // 这个 code 是和后端约定的业务 code
+
+      // 二进制数据则直接返回
+      if (responseType === "blob" || responseType === "arraybuffer") {
+        return apiData
+      }
+
       const code = apiData.code
-      // 如果没有 code, 代表这不是项目后端开发的 api
+
       if (code === undefined) {
         ElMessage.error("非本系统的接口")
         return Promise.reject(new Error("非本系统的接口"))
       }
+
       switch (code) {
         case 0:
           // 本系统采用 code === 0 来表示没有业务错误
           return apiData
         case 401:
-          // Token 过期时
           return logout()
         default:
-          // 不是正确的 code
-          ElMessage.error(apiData.message || "Error")
+          ElMessage.error(apiData.message || apiData.msg || "Error")
           return Promise.reject(new Error("Error"))
       }
     },
     (error) => {
-      // status 是 HTTP 状态码
+      const requestIdentifier = getRequestIdentifier(error.config)
+
+      if (requestIdentifier !== NOT_FOUND) {
+        requestSet.delete(requestIdentifier)
+        requestMap.delete(requestIdentifier)
+      }
+
       const status = get(error, "response.status")
+
       switch (status) {
         case 400:
           error.message = "请求错误"
           break
         case 401:
-          // Token 过期时
-          logout()
-          break
+          return logout()
         case 403:
           error.message = "拒绝访问"
           break
@@ -89,27 +145,28 @@ function createService() {
         default:
           break
       }
+
       ElMessage.error(error.message)
       return Promise.reject(error)
     }
   )
+
   return service
 }
 
-/** 创建请求方法 */
 function createRequest(service: AxiosInstance) {
   return function <T>(config: AxiosRequestConfig): Promise<T> {
     const token = getToken()
     const defaultConfig = {
       headers: {
-        // 携带 Token
-        Authorization: token ? `Bearer ${token}` : undefined,
+        token: token ? `${token}` : undefined,
         "Content-Type": "application/json"
       },
-      timeout: 5000,
-      baseURL: import.meta.env.VITE_BASE_API,
+      timeout: 500000,
+      baseURL: import.meta.env.VITE_BASE_API as string,
       data: {}
     }
+
     // 将默认配置 defaultConfig 和传入的自定义配置 config 进行合并成为 mergeConfig
     const mergeConfig = merge(defaultConfig, config)
     return service(mergeConfig)
@@ -120,3 +177,23 @@ function createRequest(service: AxiosInstance) {
 const service = createService()
 /** 用于网络请求的方法 */
 export const request = createRequest(service)
+
+/**
+ * 打印请求的详细信息
+ * @param previousRequest 上一个请求的信息
+ * @param currentTime 当前时间
+ * @param requestInterval 间隔时间(ms) 小于此时间视为重复提交
+ */
+function logRequestDetails(previousRequest: any, currentTime: number, requestInterval: number): void {
+  console.log(
+    `${previousRequest},
+    "当前时间",
+    ${currentTime},
+    "上个重复请求的时间",
+    ${previousRequest!.timestamp},
+    "间隔",
+    ${currentTime - previousRequest!.timestamp},
+    "间隔是否满足重复重复请求的定义",
+    ${currentTime - previousRequest!.timestamp <= requestInterval}`
+  )
+}
